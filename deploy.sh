4a4f8ffbfb2ba78defefa1dca05fd1a19b4538fa
Balasubramanian-d <143893846+Balasubramanian-d@users.noreply.github.com>
2024-02-16 13:26:34 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ docker login -u balasubramaniand -p dckr_pat_bLlyFbCSTx8iEbJFOrcRNRS8dWw
 if ["$GIT_BRANCH" == "origin/dev"]; then
 docker tag mynewimage balasubramaniand/dev
 docker push balasubramaniand/dev
-elif ["$GIT_BRANCH" == "origin/prod"]; then
+elif ["$GIT_BRANCH" == "origin/main"]; then
 docker tag mynewimage balasubramaniand/prod
 docker push balasubramaniand/prod
 fi
